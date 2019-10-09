71c69ec75cbefcf4f333585ec40be2ba44cf44a6
Lincoln Ferreira Campos <link7-7@hotmail.com>
2019-10-09 01:14:02 -0300

    Criando um componente filho, para separar as responsabilidades, como usar o ngFor, ngIf, tratando a propriedade vazia de cladara no ngModel, tipando o controlador input, tratando o css, tratando efeitos do css

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { from } from 'rxjs';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +11,18 @@ import { from } from 'rxjs';
 })
 export class HeroesComponent implements OnInit {
 
-  hero :Hero = {
-    id : 1, 
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero :Hero;   
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void{
+
+    this.selectedHero = hero;
+
   }
 
 }
